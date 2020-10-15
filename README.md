be316d5c845f4917839c6db464fcf1e4616eaddc
Ozgur Gunes <o.gunes@gmail.com>
2020-10-16 02:43:20 +0300

    Configuration options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sketch Plugin Analytics
 
-Google Analytics module for Sketch plugins. Respects user privacy, asks for allow to tracking statistics.
+Google Analytics module for Sketch plugins. Respects user privacy. Asks user to allow for tracking statistics, if not allowed before.
 
 ## Installation
 
@@ -13,19 +13,78 @@ npm i @ozgurgunes/sketch-plugin-analytics
 ```javascript
 import analytics from '@ozgurgunes/sketch-plugin-analytics'
 
-var eventLabel = 'My Event'
+var eventLabel = 'My Command Event'
 var eventValue = 1
-var trackingId = 'UA-1234-1'
+var trackingID = 'UA-123456-1'
+var options = {
+  eventCategory: 'My Sketch Plugin',
+  eventAction: 'My Plugin Command',
+  applicationName: 'My Sketch Plugin',
+  applicationVersion: '1.0.0',
+  applicationID: 'com.example.my-sketch-plugin',
+  dataSource: 'Sketch'
+}
 
-analytics(eventLabel, eventValue, trackingId)
+analytics(eventLabel, eventValue, trackingID, options)
+
+// All parameters are optional and could be set in JSON file.
+// So, could be used simply like;
+analytics()
+
+// or any options in the JSON could be overridden like;
+anlytics(null, null, 'UA-123456-1', { eventCategory: 'Category Override' })
+
+// or;
+anlytics(null, null, null, {
+  eventLabel: 'Event Label Override',
+  trackingID: 'UA-123456-1'
+})
+
+// even;
+anlytics('This will be used', null, null, {
+  eventLabel: 'This will not be used!'
+})
 ```
 
-### Collects:
+## Configuration File
 
-- Hit Type: `event`
-- Data Source: `Sketch Version`
-- Application Name: `Plugin Name`
-- Application Version: `Plugin Version`
-- Application ID: `Plugin Identifier`
-- Event Category: `Plugin Name`
-- Event Action: `Command Name`
+It looks for an `analytics.json` file in the `Resources` folder of the plugin. All options can be overridden by passing parameters to the module.
+
+> ⚠️ Do not forget to pass the `trackingID` to the module or set in the JSON file. Otherwise, my ID used as default :)
+
+### Example JSON
+```json
+{
+  "trackingID": "UA-123456-1",
+  "eventLabel": "My Command Event",
+  "eventValue": 1,
+  "eventCategory": "My Sketch Plugin",
+  "eventAction": "My Plugin Command",
+  "applicationName": "My Sketch Plugin",
+  "applicationVersion": "1.0.0",
+  "applicationID": "com.example.my-sketch-plugin",
+  "dataSource": "Sketch",
+  "allowDialogTitle": "Allow Google Analytics",
+  "allowDialogMessage": "Please allow My Sketch Plugin to use Google Analytics for tracking statistics."
+}
+```
+
+<a name="module_Module"></a>
+
+## Module
+Sends data to Google Analytics if allowed. Asks user to allow for tracking statistics, if not allowed before.
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [eventLabel] | <code>string</code> | Specifies the event label. |
+| [eventValue] | <code>number</code> | Specifies the event value. Must be non-negative. |
+| [trackingID] | <code>string</code> | The measurement ID / web property ID |
+| [options] | <code>Object</code> | Options to be passed to the module. |
+| [options.eventAction] | <code>string</code> | Specifies the event action. Default is                                       the running command name of the                                       plugin. |
+| [options.eventCategory] | <code>string</code> | Specifies the event category. Default                                         is the name of the plugin. |
+| [options.dataSource] | <code>string</code> | Indicates the data source of the hit.                                      Default is Sketch version. |
+| [options.applicationName] | <code>string</code> | Specifies the application name.                                           Default is the name of the plugin. |
+| [options.applicationVersion] | <code>string</code> | Specifies the application                                              version. Default is the version                                              of the plugin. |
+| [options.applicationID] | <code>string</code> | Application identifier. Default is                                         the identifier of the plugin. |
+
--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 import { settingForKey, setSettingForKey } from 'sketch/settings'
 
-export default function(eventLabel, eventValue, trackingId) {
+try {
+  var path = context.plugin.urlForResourceNamed('analytics.json').path()
+  var json = JSON.parse(
+    NSString.stringWithContentsOfFile_encoding_error(
+      path,
+      NSUTF8StringEncoding,
+      nil
+    )
+  )
+} catch (error) {
+  console.log(error)
+}
+if (!json) {
+  json = {}
+}
+
+var allowDialogTitle = json.allowDialogTitle || 'Allow Google Analytics'
+var allowDialogMessage =
+  json.allowDialogMessage ||
+  'Please allow ' +
+    context.plugin.name() +
+    ' plugin to use Google Analytics for tracking statistics.'
+
+/**
+ * @description Sends data to Google Analytics if allowed. Asks user to allow
+ *              for tracking statistics, if not allowed before.
+ *
+ * @module Module
+ * @param {string} [eventLabel] Specifies the event label.
+ * @param {number} [eventValue] Specifies the event value. Must be non-negative.
+ * @param {string} [trackingID] The measurement ID / web property ID
+ * @param {Object} [options] Options to be passed to the module.
+ * @param {string} [options.eventAction] Specifies the event action. Default is
+ *                                       the running command name of the
+ *                                       plugin.
+ * @param {string} [options.eventCategory] Specifies the event category. Default
+ *                                         is the name of the plugin.
+ * @param {string} [options.dataSource] Indicates the data source of the hit.
+ *                                      Default is Sketch version.
+ * @param {string} [options.applicationName] Specifies the application name.
+ *                                           Default is the name of the plugin.
+ * @param {string} [options.applicationVersion] Specifies the application
+ *                                              version. Default is the version
+ *                                              of the plugin.
+ * @param {string} [options.applicationID] Application identifier. Default is
+ *                                         the identifier of the plugin.
+ */
+export default function(eventLabel, eventValue, trackingID, options = {}) {
   let analyticsAllowed = settingForKey('analyticsAllowed') || false
 
   if (analyticsAllowed != true) {
@@ -8,13 +55,8 @@ export default function(eventLabel, eventValue, trackingId) {
     if (context.plugin.alertIcon()) {
       dialog.icon = context.plugin.alertIcon()
     }
-    dialog.setMessageText('Allow Google Analytics')
-    dialog.setInformativeText(
-      'Please allow ' +
-        context.plugin.name() +
-        ' ' +
-        'plugin to use Google Analytics for tracking statistics.'
-    )
+    dialog.setMessageText(allowDialogTitle)
+    dialog.setInformativeText(allowDialogMessage)
     dialog.addButtonWithTitle('Allow')
     dialog.addButtonWithTitle('Disallow')
     let response = dialog.runModal()
@@ -25,11 +67,11 @@ export default function(eventLabel, eventValue, trackingId) {
   }
 
   if (analyticsAllowed) {
-    return analytics(eventLabel, eventValue, trackingId)
+    return analytics(eventLabel, eventValue, trackingID, options)
   }
 }
 
-function analytics(eventLabel, eventValue, trackingId) {
+function analytics(eventLabel, eventValue, trackingID, options = {}) {
   let uuidKey = 'google.analytics.uuid'
   let uuid = NSUserDefaults.standardUserDefaults().objectForKey(uuidKey)
   if (!uuid) {
@@ -41,17 +83,55 @@ function analytics(eventLabel, eventValue, trackingId) {
   let payload = {
     v: 1,
     cid: uuid,
-    tid: trackingId || 'UA-5738625-2',
-    ds: 'Sketch ' + MSApplicationMetadata.metadata().appVersion,
     t: 'event',
-    an: context.plugin.name(),
-    aid: context.plugin.identifier(),
-    av: context.plugin.version(),
-    ec: context.plugin.name(),
-    ea: context.command.name(),
-    el: eventLabel || 'No Label',
-    ev: eventValue || 0
+    tid: trackingID
+      ? trackingID
+      : options.trackingID
+      ? options.trackingID
+      : json.trackingID
+      ? json.trackingID
+      : 'UA-5738625-2',
+    ds: options.dataSource
+      ? options.dataSource
+      : json.dataSource
+      ? json.dataSource
+      : 'Sketch ' + MSApplicationMetadata.metadata().appVersion,
+    an: options.applicationName
+      ? options.applicationName
+      : json.applicationName
+      ? json.applicationName
+      : context.plugin.name(),
+    aid: options.applicationID
+      ? options.applicationID
+      : json.applicationID
+      ? json.applicationID
+      : context.plugin.identifier(),
+    av: options.applicationVersion
+      ? options.applicationVersion
+      : json.applicationVersion
+      ? json.applicationVersion
+      : context.plugin.version(),
+    ec: options.eventCategory
+      ? options.eventCategory
+      : json.eventCategory
+      ? json.eventCategory
+      : context.plugin.name(),
+    ea: options.eventAction
+      ? options.eventAction
+      : json.eventAction
+      ? json.eventAction
+      : context.command.name()
   }
+  let el = eventLabel || options.eventLabel || json.eventLabel
+  let ev = eventValue || options.eventValue || json.eventValue
+  if (el) {
+    payload.el = el
+  }
+  if (ev) {
+    payload.ev = ev
+  }
+
+  console.log(payload)
 
   let url = NSURL.URLWithString(
     NSString.stringWithFormat(
